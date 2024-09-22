bd4f83421131d99120dfd2c0d32be46c2f2faf45
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-22 21:11:59 +0300

    integrate cubit with product_list_view_body.dart

--- lib/features/product_list/presentation/views/widgets/product_list_view_body.dart
+++ lib/features/product_list/presentation/views/widgets/product_list_view_body.dart
@@ -1,5 +1,9 @@
+import 'package:elevate_flutter_task/core/utils/widgets/custom_error_widget.dart';
+import 'package:elevate_flutter_task/core/utils/widgets/custom_loading_indicator.dart';
+import 'package:elevate_flutter_task/features/product_list/presentation/manager/product_list_cubit/product_list_cubit.dart';
 import 'package:elevate_flutter_task/features/product_list/presentation/views/widgets/custom_product.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductListViewBody extends StatelessWidget {
   const ProductListViewBody({super.key});
@@ -8,15 +12,29 @@ class ProductListViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16,
-          childAspectRatio: .65,
-        ),
-        itemBuilder: (context, index) => const CustomProduct(),
-        itemCount: 10,
+      child: BlocBuilder<ProductListCubit, ProductListState>(
+        builder: (context, state) {
+          if (state is ProductListSuccess) {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: .65,
+              ),
+              itemBuilder: (context, index) => CustomProduct(
+                productModel: state.products[index],
+              ),
+              itemCount: state.products.length,
+            );
+          } else if (state is ProductListFailure) {
+            return CustomErrorWidget(
+              errMessage: state.errMessage,
+            );
+          } else {
+            return const CustomLoadingIndicator();
+          }
+        },
       ),
     );
   }
