49322e3fb366df0f4e4a3f0146738ac6e2a6a69d
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-22 19:13:29 +0300

    bind api_services.dart with product_list_repo_implementation.dart

--- lib/features/product_list/data/repos/product_list_repo_implementation.dart
+++ lib/features/product_list/data/repos/product_list_repo_implementation.dart
@@ -1,12 +1,33 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:elevate_flutter_task/core/errors/failure.dart';
+import 'package:elevate_flutter_task/core/utils/api_services.dart';
 import 'package:elevate_flutter_task/features/product_list/data/models/product_model.dart';
 import 'package:elevate_flutter_task/features/product_list/data/repos/product_list_repo.dart';
 
-class ProductListRepoImplementation implements ProductListRepo{
+class ProductListRepoImplementation implements ProductListRepo {
+  final ApiService apiService;
+
+  ProductListRepoImplementation(this.apiService);
+
   @override
-  Future<Either<Failure, List<ProductModel>>> fetchProductsItems() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<ProductModel>>> fetchProductsItems() async {
+    try {
+      var data = await apiService.get();
+      List<ProductModel> products =
+          (data as List).map((item) => ProductModel.fromJson(item)).toList();
+      return right(products);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
