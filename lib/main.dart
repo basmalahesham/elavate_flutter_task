4abf793e9cd330b5863c34455562280120f690e7
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-22 21:10:40 +0300

    provide & trigger cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:elevate_flutter_task/core/utils/service_locator.dart';
+import 'package:elevate_flutter_task/features/product_list/data/repos/product_list_repo_implementation.dart';
+import 'package:elevate_flutter_task/features/product_list/presentation/manager/product_list_cubit/product_list_cubit.dart';
 import 'package:elevate_flutter_task/features/product_list/presentation/views/product_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setUpServiceLocator();
   runApp(const MyApp());
 }
 
@@ -10,9 +15,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ProductListView(),
+    return BlocProvider(
+      create: (context) => ProductListCubit(
+        getIt.get<ProductListRepoImplementation>(),
+      )..fetchProducts(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: ProductListView(),
+      ),
     );
   }
 }
